9ed233f91f2552956886ce117a826fe5a3c53663
C4TInD <xavihurtadocanadas@gmail.com>
2015-06-03 18:25:18 +0200

    Commit Seguretat Pre Imatges

---
+++ ArmyHammerProject/Models/Imatge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmyHammerProject.Models
+{
+    public class Imatge
+    {
+        public int ImatgeId { get; set; }
+        [StringLength(255)]
+        public string NomImatge { get; set; }
+        public FileType FileType { get; set; }
+        public int UsuariID { get; set; }
+        public virtual Miniatura Miniatura { get; set; }
+    }
+}
